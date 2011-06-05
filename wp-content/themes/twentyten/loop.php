01bf4096f5f61ac75655284e5e94a02d711c745e
willpearson <wpearson01@gmail.com>
2011-06-05 21:37:01 +0100

    Removed bookmark/comment and moved categories to top

--- wp-content/themes/twentyten/loop.php
+++ wp-content/themes/twentyten/loop.php
@@ -132,6 +132,12 @@
 
 			<div class="entry-meta">
 				<?php twentyten_posted_on(); ?>
+				<?php if ( count( get_the_category() ) ) : ?>
+					<span class="cat-links">
+						<?php printf( __( '<span class="%1$s"> in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+					</span>
+					<!-- <span class="meta-sep">|</span> -->
+				<?php endif; ?>
 			</div><!-- .entry-meta -->
 
 	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
@@ -146,12 +152,12 @@
 	<?php endif; ?>
 
 			<div class="entry-utility">
-				<?php if ( count( get_the_category() ) ) : ?>
+				<!-- <?php if ( count( get_the_category() ) ) : ?>
 					<span class="cat-links">
 						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
 					</span>
 					<span class="meta-sep">|</span>
-				<?php endif; ?>
+				<?php endif; ?> -->
 				<?php
 					$tags_list = get_the_tag_list( '', ', ' );
 					if ( $tags_list ):
@@ -159,10 +165,10 @@
 					<span class="tag-links">
 						<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
 					</span>
-					<span class="meta-sep">|</span>
+					<!-- <span class="meta-sep">|</span> -->
 				<?php endif; ?>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+				<!-- <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span> -->
+				<!-- <?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?> -->
 			</div><!-- .entry-utility -->
 		</div><!-- #post-## -->
 
