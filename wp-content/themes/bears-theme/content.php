5f50142e275cb7a93cf2286f44a33042eac5ac73
willpearson <wpearson01@gmail.com>
2011-09-03 16:13:57 +0100

    styling to general layout and featured posts

--- wp-content/themes/bears-theme/content.php
+++ wp-content/themes/bears-theme/content.php
@@ -13,7 +13,7 @@
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
 					<h2 class="entry-title"><?php the_category(' '); ?>// <a class="entry-title-post" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-					<!-- <h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3> -->
+					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
 			<?php else : ?>
 			<h1 class="entry-title"><?php the_category(' '); ?>// <a class="entry-title-post" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
@@ -37,6 +37,9 @@
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
 		<?php else : ?>
+		<?php if ( is_sticky() ) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_post_thumbnail( 'large'); ?></a>
+		<?php endif; ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
--- wp-content/themes/bears-theme/functions.php
+++ wp-content/themes/bears-theme/functions.php
@@ -1,12 +1,20 @@
 <?php
-function bears_excerpt_length( $length ) {
-	return 33;
+if ( ! function_exists( 'twentyeleven_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own twentyeleven_posted_on to override in a child theme
+ *
+ * @since Twenty Eleven 1.0
+ */
+function twentyeleven_posted_on() {
+	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'twentyeleven' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date( 'l, j F Y' ) ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		sprintf( esc_attr__( 'View all posts by %s', 'twentyeleven' ), get_the_author() ),
+		esc_html( get_the_author() )
+	);
 }
-add_filter( 'excerpt_length', 'bears_excerpt_length');
-
-function new_excerpt_more($more) {
-	return '[.....]';
-}
-add_filter('excerpt_more', 'new_excerpt_more');
-
-?>
+endif;
